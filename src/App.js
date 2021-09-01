f6ef03d972750030372f7d391412a56ec2497001
Risqian <risqian99@gmail.com>
2021-09-01 23:51:04 +0700

    LandingPages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import 'assets/scss/style.scss'
-import { Router, Route, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
+import 'assets/scss/style.scss'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-        </Switch>
+        {/* <Switch> */}
+        <Route path="/" component={LandingPage} />
+        {/* </Switch> */}
       </Router>
     </div>
   );
